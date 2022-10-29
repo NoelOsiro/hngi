c6e0f4e36aa75f061797fa8eb429ee9382129419
Noel Otieno Osiro <noel.osiro@azubiafrica.org>
2022-10-29 18:39:18 +0300

    Created React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container,Row, } from 'reactstrap';
 import './App.css';
+import profpic from './assets/img/profpic.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container > 
+      <div className='main-section p-4'>
+        <Row>
+          <div className='profileImg '>
+            <img src={profpic} alt='' id='profile_img'/>
+          </div>
+        </Row>
+
+      </div>
+    </Container>
   );
 }
 

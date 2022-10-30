d8d7ab7c4cfe3a6259234b0a3d54065ffa9f46ee
Noel Otieno Osiro <noel.osiro@azubiafrica.org>
2022-10-30 14:13:25 +0300

    Configured Mobile UI break points

--- src/Components/LinkSection/LinkSection.tsx
+++ src/Components/LinkSection/LinkSection.tsx
@@ -8,7 +8,7 @@ const LinkSection = () => {
   return (
     <Row className='mt-5 p-3'>
       {Links.map((link)=>(
-        <LinkButton href={link.href} text={link.text} id={link.text}/>
+        <LinkButton key={link.text} href={link.href} text={link.text} id={link.text}/>
       ))}
     </Row>
   )

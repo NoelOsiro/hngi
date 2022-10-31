c63bcc73909d81a2747b7f14ee1b3295cf0517a0
Noel Otieno Osiro <noel.osiro@azubiafrica.org>
2022-10-31 11:43:18 +0300

    Fixed share button

--- src/Components/ProfileSection/avatar.tsx
+++ src/Components/ProfileSection/avatar.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import { ReactComponent as ShareDefault } from '../../assets/img/share.svg';
-import { ReactComponent as ShareHover } from '../../assets/img/sharehover.svg';
 
 const Share = () => {
-    const [isShown, setIsShown] = React.useState(false);
   return (
-    <div className='avatar_share'
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}>
-        <div className='share_06'>
-            {isShown? (
-                <ShareHover/>
-            ):(<ShareDefault />)}
-          
+    <div className='avatar_share'>
+        <div className='shareButton'>
+            
         </div>
-      </div>
+          
+    </div>
   )
 }
 
